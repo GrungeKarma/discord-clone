bb182ee8e42d72dfd0a170e4c241c564e39cd5e0
Anthony Lopez <grungekarma@gmail.com>
2021-08-08 12:37:08 -0500

    add sidebar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+    return ( <div className="app">
+        <h1>Let's create a Discord Clone</h1>
+
+        {/*Sidebar */}
+        <Sidebar />
+
+        {/*Chat*/}
+    </div>
+    )
+
+}
+
+export default App;
---
+++ src/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+      <h2>I am the Sidebar</h2>
+    </div>
+  )
+}
+
+export default Sidebar
